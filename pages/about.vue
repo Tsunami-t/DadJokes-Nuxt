f8d4a4737b2bc5f97b1f4c93a0ca763caa8b39cc
Tsunami-t <tsunamiorgasm@protonmail.com>
2020-09-09 18:26:54 +0100

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an epic app that displays funny dad jokes.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About Us',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Prank Your Son'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h2>
+      Welcome to the most epic DadJokes on the web.
+    </h2>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<script>
+export default {
+    head() {
+        return {
+            title: 'About Us',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Prank Your Son'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+</style>
